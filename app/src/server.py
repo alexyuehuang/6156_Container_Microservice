f45cd4a43e99188171a8f46c8fe9327b744d9234
icyLRH <1224787558@qq.com>
2022-12-20 16:14:34 -0500

    update

--- app/src/server.py
+++ app/src/server.py
@@ -152,4 +152,5 @@ def health():
 
 
 if __name__ == '__main__':
-    server.run(debug=True, host='0.0.0.0', port=5000)
+    # server.run(debug=True, host='0.0.0.1', port=5000)
+    server.run(debug=True)
